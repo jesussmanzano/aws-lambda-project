db9513a9e19776dd96b65b348eccea768bdd9997
jesussmanzano <susej.sm@gmail.com>
2024-08-28 19:05:58 -0600

    Second commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module crud-lambda
+
+go 1.23.0
+
+require github.com/aws/aws-lambda-go v1.47.0 // indirect
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Println("Lambda crud")
+
+	response := events.APIGatewayProxyResponse{
+		StatusCode: 200,
+	}
+
+	return response, nil
+
+}
